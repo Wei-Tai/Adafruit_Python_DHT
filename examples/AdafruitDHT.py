d682370f25b71c517538481eb08e59f5412895d4
Wei-Tai <74525062+Wei-Tai@users.noreply.github.com>
2020-11-16 15:35:47 +0800

    Update AdafruitDHT.py

--- examples/AdafruitDHT.py
+++ examples/AdafruitDHT.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # Copyright (c) 2014 Adafruit Industries
 # Author: Tony DiCola
 
@@ -19,16 +19,16 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-
-#/usr/bin/python3
-import Adafruit_DHT
-import time
 import sys
 import http.client as http
 import urllib
 import json
-deviceId = “DqT48OAC“ 
-deviceKey = “qJoJaGKdnA6gtKu0“ 
+import time
+import Adafruit_DHT
+
+deviceId = 'DVDsquZx'
+deviceKey = '4v9qgv9fXNUYKudK' 
+
 def post_to_mcs(payload): 
 	headers = {"Content-type": "application/json", "deviceKey": deviceKey} 
 	not_connected = 1 
@@ -39,13 +39,14 @@ def post_to_mcs(payload):
 			not_connected = 0 
 		except (http.HTTPException, socket.error) as ex: 
 			print ("Error: %s" % ex)
- 			time.sleep(10)
+			time.sleep(10)
 			 # sleep 10 seconds 
 	conn.request("POST", "/mcs/v2/devices/" + deviceId + "/datapoints", json.dumps(payload), headers) 
 	response = conn.getresponse() 
 	print( response.status, response.reason, json.dumps(payload), time.strftime("%c")) 
 	data = response.read() 
 	conn.close() 
+ 
 
 
 # Parse command line parameters.
@@ -76,18 +77,13 @@ if humidity is not None and temperature is not None:
 else:
     print('Failed to get reading. Try again!')
     sys.exit(1)
-while true:
-h0, t0= Adafruit_DHT.read_retry(sensor, pin)
-if h0 is not None and t0 is not None:
-print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(t0, h0))
-
-	payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},
-		{"dataChnId":"Temperature","values":{"value":t0}}]} 
-	post_to_mcs(payload)
-	time.sleep(10) 
-
-else:
-print('Failed to get reading. Try again!')
-sys.exit(1)
-
-
+while(1):
+	h0,t0 = Adafruit_DHT.read_retry(sensor,pin)
+	if h0 is not None and t0 is not None:
+		print('Temp={0:0.1f}* Humidity={1:0.1f}%'.format(t0,h0))
+		payload = {"datapoints":[{"dataChnId":"Humidity","values":{"value":h0}},{"dataChnId":"Temperature","values":{"value":t0}}]} 
+		post_to_mcs(payload)
+		time.sleep(10)
+	else:
+		print('Failed to get reading. Try again!')
+		sys.exit(1)
